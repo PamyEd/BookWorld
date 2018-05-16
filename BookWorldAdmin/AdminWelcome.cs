bdb9a504c3110ebc30404ea063eb96256d7c6dee
Pamela Edison <Pamela-Edison@live.nmit.ac.nz>
2018-05-16 13:08:00 +1200

    Add project files.

---
+++ BookWorldAdmin/AdminWelcome.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookWorldAdmin
+{
+    public partial class AdminWelcome : Form
+    {
+        public AdminWelcome()
+        {
+            InitializeComponent();
+        }
+    }
+}
